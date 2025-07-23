7196cb560e84e5f07ecb0c8b1d20cfb6d8d15380
chetan-shirahatti099 <chetan.shirahatti@zeksta.com>
2025-07-23 23:40:48 +0530

    Fix case-sensitive path for ThemeContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "/src/context/ThemeContext.jsx";
+import { ThemeProvider } from "/src/context/Themecontext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
