c11ba855a8f865b7bc9c1f0e50e12062c2527b8f
chetan-shirahatti099 <chetan.shirahatti@zeksta.com>
2025-07-24 00:18:45 +0530

    Imported proper image path from projects for deploying

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { ExternalLink } from "lucide-react";
+import knacproimg from "../assets/Images/knacpro1.png";
+import Gandhadagudi from "../assets/Images/GG.png";
+import buchipay from "../assets/Images/buchipay.png";
+import loginapp from "../assets/Images/whitebg1.jpg";
+import weatherapp from "../assets/Images/whetherapp.png";
+import tictactoe from "../assets/Images/tictactoe.png";
 
 const projects = [
   {
     title: "KnacproSolutions Website",
-    image: "/src/assets/Images/knacpro1.png",
+    image: knacproimg,
     description:
       "A professional website for a software development company, showcasing services and portfolio, built with React and Tailwind CSS.",
     link: "https://www.knacprosolutions.in/",
@@ -12,7 +18,7 @@ const projects = [
 
   {
     title: "Gandhadagudi Garden Website",
-    image: "/src/assets/Images/GG.png",
+    image: Gandhadagudi,
     description:
       "Static yet elegant landing page for a village-based eco-tourism garden, built with HTML, Tailwind CSS, and React.",
     link: "https://www.gandhadagudigardenbyahatti.in/",
@@ -20,28 +26,28 @@ const projects = [
 
   {
     title: "Buchipay Fintech App",
-    image: "/src/assets/Images/buchipay.png",
+    image: buchipay,
     description:
       "A digital payment app built with Flutter, integrated with Firebase and REST APIs for real-time financial transactions.",
     link: "https://play.google.com/store/apps/details?id=com.buchipay.app",
   },
   {
     title: "Flutter Login App",
-    image: "/src/assets/Images/whitebg3.jpg",
+    image: loginapp,
     description:
       "Simple login application built with Flutter and Firebase featuring light/dark mode toggle and secure email/password authentication.",
     link: "https://github.com/chetanshirahatti/Simple_Login_App_Flutter",
   },
   {
     title: "Weather App",
-    image: "/src/assets/Images/whetherapp.png",
+    image: weatherapp,
     description:
       "A weather application built using HTML, CSS, and JS that fetches data from OpenWeatherMap API to display current weather.",
     link: "https://github.com/chetanshirahatti/WeatherAPI-App",
   },
   {
     title: "Tic Tac Toe Game",
-    image: "/src/assets/Images/tictactoe.png",
+    image: tictactoe,
     description:
       "A classic Tic Tac Toe game built with HTML, CSS, and JavaScript. Play against a friend or computer.",
     link: "https://github.com/chetanshirahatti/Tic--Tac-Toe",
